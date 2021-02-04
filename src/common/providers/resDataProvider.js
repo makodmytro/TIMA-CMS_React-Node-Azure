11534b26bf2dc5fc9f1211ac49bd8654896fc2bd
Yael Brodsky <brodskyy88@gmail.com>
2021-02-04 18:27:01 -0300

    question create

--- src/common/providers/resDataProvider.js
+++ src/common/providers/resDataProvider.js
@@ -215,14 +215,6 @@ const resDataProvider = {
       body: JSON.stringify({ approved: true }),
     });
   },
-  createAnswerWithQuestions: async (resource, params) => {
-    const { json } = await httpClient(`${baseApi}/answers`, {
-      method: 'POST',
-      body: JSON.stringify(params.data),
-    });
-
-    return { data: json };
-  },
 };
 
 export default resDataProvider;
--- src/questions/QuestionCreate.js
+++ src/questions/QuestionCreate.js
@@ -2,14 +2,11 @@ import React from 'react';
 import {
   Create,
   SimpleForm,
-  useNotify,
-  useDataProvider,
   required,
-  useRedirect,
   ReferenceInput,
   SelectInput,
 } from 'react-admin';
-import { useField, Form } from 'react-final-form'; // eslint-disable-line
+import { useField } from 'react-final-form'; // eslint-disable-line
 import { connect } from 'react-redux';
 import CustomTopToolbar from '../common/components/custom-top-toolbar';
 import { PlayableTextInput } from '../common/components/playable-text';
@@ -67,52 +64,32 @@ const FormFields = (props) => {
 };
 
 const QuestionCreate = ({ dispatch, languages, ...props }) => {
-  const dataProvider = useDataProvider();
-  const notify = useNotify();
-  const redirect = useRedirect();
-
-  const onSubmit = async (values) => {
-    try {
-      const { answer_text: answerText, ...rest } = values;
-
-      if (answerText && !rest.fk_answerId) {
-        const { data } = await dataProvider.createAnswerWithQuestions(null, {
-          data: {
-            text: answerText,
-            fk_topicId: rest.fk_topicId,
-            fk_languageId: rest.fk_languageId,
-            questions: [{
-              ...rest,
-            }],
-          },
-        });
-
-        redirect(`/answers/${data.id}`);
-      } else {
-        const { data } = await dataProvider.create('questions', {
-          data: rest,
-        });
-
-        redirect(`/questions/${data.id}`);
-      }
-
-      notify('The question has been created');
-    } catch (err) {
-      if (err.body && err.body.message) {
-        notify(err.body.message, 'error');
-      }
-
-      throw err;
-    }
-  };
-
   return (
     <>
       <Create
         {...props}
         actions={<CustomTopToolbar />}
+        transform={async (data) => {
+          const { answer_text: answerText, fk_answerId, ...rest } = data;
+
+          if (fk_answerId) {
+            return {
+              ...rest,
+              fk_answerId,
+            };
+          }
+
+          if (answerText) {
+            return {
+              ...rest,
+              answerText,
+            };
+          }
+
+          return rest;
+        }}
       >
-        <SimpleForm save={onSubmit}>
+        <SimpleForm>
           <FormFields languages={languages} />
         </SimpleForm>
       </Create>
