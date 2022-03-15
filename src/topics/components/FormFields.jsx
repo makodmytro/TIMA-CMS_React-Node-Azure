b3e19851caa03c3b6ffabae483fb858de367ac26
Yael Brodsky <brodskyy88@gmail.com>
2022-03-15 11:31:00 -0300

    topics hide fields and sidemenu hide items

--- src/topics/components/FormFields.jsx
+++ src/topics/components/FormFields.jsx
@@ -11,6 +11,16 @@ import TopicImage from './Image';
 import { PlayableTextInput } from '../../common/components/playable-text';
 import { useDisabledEdit, useIsAdmin } from '../../hooks';
 
+const HIDE_FIELDS_TOPICS = process.env.REACT_APP_HIDE_FIELDS_TOPICS ? process.env.REACT_APP_HIDE_FIELDS_TOPICS.split(',') : [];
+
+const HiddenField = ({ children, fieldName }) => {
+  if (HIDE_FIELDS_TOPICS.includes(fieldName)) {
+    return null;
+  }
+
+  return children;
+};
+
 export const Advanced = (props) => {
   const translate = useTranslate();
 
@@ -20,11 +30,11 @@ export const Advanced = (props) => {
         {translate('misc.advanced')}
       </Typography>
       <TextInput
-        source="topicKey"
+        source="topicfieldName"
         record={props.record}
         fullWidth
         disabled={props.disabled === true}
-        label="resources.topics.fields.topicKey"
+        label="resources.topics.fields.topicfieldName"
       />
     </>
   );
@@ -46,8 +56,8 @@ export const Qna = (props) => {
         disabled={props.disabled === true}
       />
       <TextInput
-        source="qnaSubscriptionKey"
-        label="resources.topics.fields.qnaSubscriptionKey"
+        source="qnaSubscriptionfieldName"
+        label="resources.topics.fields.qnaSubscriptionfieldName"
         record={props.record}
         fullWidth
         disabled={props.disabled === true}
@@ -77,61 +87,77 @@ const FormFields = (props) => {
 
   return (
     <>
-      <PlayableTextInput
-        source="name"
-        validate={required()}
-        fullWidth
-        lang={getLang}
-        disabled={disabled && !admin}
-        record={props?.record}
-        label="resources.topics.fields.name"
-      />
-      <PlayableTextInput
-        source="welcomeText"
-        fullWidth
-        rows="4"
-        multiline
-        lang={getLang}
-        disabled={disabled && !admin}
-        record={props?.record}
-        label="resources.topics.fields.welcomeText"
-      />
-      <ReferenceInput
-        validate={required()}
-        source="fk_languageId"
-        reference="languages"
-        label="resources.topics.fields.language"
-        fullWidth
-        disabled={disabled && !admin}
-      >
-        <SelectInput
-          optionText="name"
+      <HiddenField fieldName="name">
+        <PlayableTextInput
+          source="name"
+          validate={required()}
+          fullWidth
+          lang={getLang}
+          disabled={disabled && !admin}
+          record={props?.record}
+          label="resources.topics.fields.name"
         />
-      </ReferenceInput>
-      <TextInput
-        source="topicImageUrl"
-        fullWidth
-        disabled={disabled && !admin}
-        label="resources.topics.fields.image"
-      />
+      </HiddenField>
+      <HiddenField fieldName="welcomeText">
+        <PlayableTextInput
+          source="welcomeText"
+          fullWidth
+          rows="4"
+          multiline
+          lang={getLang}
+          disabled={disabled && !admin}
+          record={props?.record}
+          label="resources.topics.fields.welcomeText"
+        />
+      </HiddenField>
+      <HiddenField fieldName="fk_languageId">
+        <ReferenceInput
+          validate={required()}
+          source="fk_languageId"
+          reference="languages"
+          label="resources.topics.fields.language"
+          fullWidth
+          disabled={disabled && !admin}
+        >
+          <SelectInput
+            optionText="name"
+          />
+        </ReferenceInput>
+      </HiddenField>
+      <HiddenField fieldName="topicImageUrl">
+        <TextInput
+          source="topicImageUrl"
+          fullWidth
+          disabled={disabled && !admin}
+          label="resources.topics.fields.image"
+        />
+      </HiddenField>
       <TopicImage {...props} />
-      <ReferenceInput
-        source="fk_parentTopicId"
-        reference="topics"
-        label="resources.topics.fields.fk_parentTopicId"
-        fullWidth
-        disabled={props.disabled === true}
-        allowEmpty
-      >
-        <SelectInput
-          optionText="name"
+      <HiddenField fieldName="fk_parentTopicId">
+        <ReferenceInput
+          source="fk_parentTopicId"
+          reference="topics"
+          label="resources.topics.fields.fk_parentTopicId"
+          fullWidth
+          disabled={props.disabled === true}
           allowEmpty
-          emptyText="None"
-        />
-      </ReferenceInput>
-      <TextInput source="level" label="resources.topics.fields.level" fullWidth disabled />
-      <Advanced source="topicKey" disabled={disabled && !admin} />
-      <Qna {...props} disabled={disabled && !admin} />
+        >
+          <SelectInput
+            optionText="name"
+            allowEmpty
+            emptyText="None"
+          />
+        </ReferenceInput>
+      </HiddenField>
+      <HiddenField fieldName="level">
+        <TextInput source="level" label="resources.topics.fields.level" fullWidth disabled />
+      </HiddenField>
+      <HiddenField fieldName="topicfieldName">
+        <Advanced source="topicfieldName" disabled={disabled && !admin} />
+      </HiddenField>
+      <HiddenField fieldName="qna">
+        <Qna {...props} disabled={disabled && !admin} />
+      </HiddenField>
     </>
   );
 };
