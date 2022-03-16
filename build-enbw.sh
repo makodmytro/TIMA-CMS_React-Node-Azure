ffd9bfb30a78108419ae9df3d230c82a940ad81a
Ron Tamerin <ron.tamerin.ext@motius.de>
2022-03-16 10:49:51 +0100

    update build for enbw

--- build-enbw.sh
+++ build-enbw.sh
@@ -1,5 +1,4 @@
 export REACT_APP_BASE_API=https://qnaeditor-backend1.azurewebsites.net/api
 export TIMA_ENVIRONMENT=ENBW
 export REACT_APP_SELECT_TOPIC_LEVELS=1
-yarn run build
-rm ~/Documents/cms-build/* -r && cp build/* ~/Documents/cms-build -r
+yarn run build && rm ~/Documents/cms-build/* -r && cp build/* ~/Documents/cms-build -r
