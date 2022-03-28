4e9762714502f2d852b5a4dae8de38a835a13820
Ron Tamerin <ron.tamerin.ext@motius.de>
2022-03-28 14:51:21 +0200

    update build

--- build-enbw.sh
+++ build-enbw.sh
@@ -5,4 +5,8 @@ export REACT_APP_TOPICS_TREE_CHILD_COLOR="498ca752"
 export REACT_APP_TOPICS_ENABLE_TREE_LIST=1
 export QUESTIONS_TREE_CHILD_COLOR="498ca752"
 export REACT_APP_QUESTIONS_ENABLE_TREE_LIST=1
+export REACT_APP_HIDE_FIELDS_TOPICS="welcomeText,topicImageUrl,topicKey,topicImageUrl"
+export REACT_APP_HIDE_MENU_ITEMS=stats/sessions
+export REACT_APP_HIDE_FIELDS_ANSWERS="spokenText,media"
+export REACT_APP_HIDE_SPEECH_FIELD=1
 yarn run build && rm ~/Documents/cms-build/* -r && cp build/* ~/Documents/cms-build -r
