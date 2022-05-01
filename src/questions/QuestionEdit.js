331adfe4b538e9355684999ea8eeeb23be15e40a
Ron Tamerin <ron.tamerin.ext@motius.de>
2022-05-01 13:29:34 +0200

    remove parent question field

--- src/questions/QuestionEdit.js
+++ src/questions/QuestionEdit.js
@@ -248,17 +248,6 @@ const FormFields = ({
         }}
         disabled={disableEdit}
       />
-      <ReferenceInput
-        label="resources.questions.fields.fk_parentQuestionId"
-        source="fk_parentQuestionId"
-        reference="questions"
-        filter={{ fk_topicId: fkTopicId }}
-        disabled={!fkTopicId}
-        fullWidth
-        allowEmpty
-      >
-        <SelectInput optionText="text" emptyText="None" />
-      </ReferenceInput>
     </>
   );
 };
