286312a0677468db4d91f21109d95e95f4f19ab3
makodmytro <makodmytro@gmail.com>
2022-07-28 13:00:51 +0200

    fix delete permission

--- src/questions/components/list-dropdown-menu.jsx
+++ src/questions/components/list-dropdown-menu.jsx
@@ -50,7 +50,7 @@ const DropdownMenu = ({
         variant="contained"
         color="secondary"
         size="small"
-        disabled={disableEdit && disableDelete}
+        disabled={disableDelete}
       >
         {translate('misc.actions')} <ExpandIcon />
       </Button>
