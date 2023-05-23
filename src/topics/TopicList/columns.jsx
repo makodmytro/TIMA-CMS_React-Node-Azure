57b6b86159583d2847cc3274411ca30a6f53c104
makodmytro <makodmytro@gmail.com>
2023-05-23 21:57:19 +0200

    update last content update

--- src/topics/TopicList/columns.jsx
+++ src/topics/TopicList/columns.jsx
@@ -51,7 +51,7 @@ const columns = [
   },
   {
     key: 'updatedAt',
-    el: (<DateField source="updatedAt" showTime />),
+    el: (<DateField source="contentLastUpdatedAt" showTime />),
   },
   {
     key: 'lastSyncAt',
